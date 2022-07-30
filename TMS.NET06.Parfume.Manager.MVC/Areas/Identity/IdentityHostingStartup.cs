378fca0f496d201fd369cd51f3a018366c5969ac
volha-77 <volha-77@mail.ru>
2022-07-30 13:11:56 +0300

    add React project

---
+++ TMS.NET06.Parfume.Manager.MVC/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TMS.NET06.Parfume.Manager.MVC.Data;
+
+[assembly: HostingStartup(typeof(TMS.NET06.Parfume.Manager.MVC.Areas.Identity.IdentityHostingStartup))]
+namespace TMS.NET06.Parfume.Manager.MVC.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
